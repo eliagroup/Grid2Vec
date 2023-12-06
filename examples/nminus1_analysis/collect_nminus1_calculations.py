f6f65fa4e79245106f024ea53802a06563320c11
Sam Mason <sam@Sams-MBP-2.fritz.box>
2023-12-06 14:58:57 +0100

    fixing examples to work with data with multiple chronics, and refactoring for simplicity

--- examples/nminus1_analysis/collect_nminus1_calculations.py
+++ examples/nminus1_analysis/collect_nminus1_calculations.py
@@ -5,46 +5,51 @@ import warnings
 from multiprocessing import Pool, cpu_count
 from multiprocessing.pool import AsyncResult
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import numpy as np
 
-from grid2vec.env import make_env, vector_reset, vector_step
+from grid2vec.env import make_env, vector_reset
 from grid2vec.grid import Grid, load_grid
+from grid2vec.nminus1_definition import NMinus1Definition
 from grid2vec.solver_interface import compute_obs
 
 warnings.simplefilter("ignore", category=FutureWarning)
 
 
 def filter_loading_per_failure_to_reward_elements(
-    loading_per_failure: np.ndarray, reward_elements: np.ndarray
+    loading_per_failure: np.ndarray, nminus1_elements: np.ndarray
 ) -> np.ndarray:
     """Filters the loading per failure array to only contain the elements that are
     relevant for the reward calculation.
 
     Args:
         loading_per_failure (np.ndarray): loading_per_failure array for given asset type from nminus1 calculation
-        reward_elements (np.ndarray): _for_reward array for given asset type
+        nminus1_elements (np.ndarray): array of elements that are relevant for the nminus1 calculation
 
     Returns:
         np.ndarray: filtered array with shape (n_envs, n_reward_elements, n_reward_elements)
     """
-    return loading_per_failure[:, :, reward_elements]
+    return loading_per_failure[:, :, nminus1_elements]
 
 
-def run_nminus1(grid: Grid, steps: List[int], destination_path: Path) -> None:
+def run_nminus1(
+    grid: Grid, chronic_steps: List[Tuple[int, int]], destination_path: Path
+) -> None:
     """Runs a powerflow analysis for a given grid and a given set of timesteps. Each
     step is saved as a numpy array in the destination path.
 
     Args:
         grid (Grid): loaded Grid2Vec grid
-        steps (Iterable[int]): List-like object of integer timesteps to run the powerflow analysis for
+        chronic_steps (List[Tuple[int, int]]): list of tuples of the form (chronic, step) to run the powerflow analysis for
         destination_path (Path): path to save the results to
     """
-    print(f"Process running nminus1 for steps {steps[0]} to {steps[-1]}")
-    env = make_env(grid, 1)
-    env = vector_reset(env)
-    env = vector_step(env, step_time=int(steps[0]))
+    env = make_env(grid, n_envs=len(chronic_steps))
+
+    chronics = np.array([chronic for chronic, _ in chronic_steps])
+    steps = np.array([step for _, step in chronic_steps])
+
+    env = vector_reset(env, target_chronics=chronics, target_timesteps=steps)
 
     keys_to_keep = [
         "converged",
@@ -53,28 +58,40 @@ def run_nminus1(grid: Grid, steps: List[int], destination_path: Path) -> None:
         "trafo3w_loading_per_failure",
         "trafo_loading_per_failure",
     ]
+    obs = compute_obs(env)
+    obs_smaller = {key: obs[key] for key in keys_to_keep}
 
-    for step in steps:
-        obs = compute_obs(env)
-        env = vector_step(env)
-        obs_smaller = {key: obs[key] for key in keys_to_keep}
-        obs_smaller[
-            "line_loading_per_failure"
-        ] = filter_loading_per_failure_to_reward_elements(
-            obs_smaller["line_loading_per_failure"], grid.line_for_reward
+    if grid.nminus1_definition is None:
+        raise ValueError(
+            "No nminus1 definition found. Please re-run the analysis with nminus1=True"
         )
-        obs_smaller[
-            "trafo_loading_per_failure"
-        ] = filter_loading_per_failure_to_reward_elements(
-            obs_smaller["trafo_loading_per_failure"], grid.trafo_for_reward
-        )
-        obs_smaller[
-            "trafo3w_loading_per_failure"
-        ] = filter_loading_per_failure_to_reward_elements(
-            obs_smaller["trafo3w_loading_per_failure"], grid.trafo3w_for_reward
-        )
+    else:
+        nminus1_definition: NMinus1Definition = grid.nminus1_definition
 
-        np.save(destination_path / f"{int(step)}.npy", obs_smaller)
+    obs_smaller[
+        "line_loading_per_failure"
+    ] = filter_loading_per_failure_to_reward_elements(
+        obs_smaller["line_loading_per_failure"], nminus1_definition.line_mask
+    )
+    obs_smaller[
+        "trafo_loading_per_failure"
+    ] = filter_loading_per_failure_to_reward_elements(
+        obs_smaller["trafo_loading_per_failure"], nminus1_definition.trafo_mask
+    )
+    obs_smaller[
+        "trafo3w_loading_per_failure"
+    ] = filter_loading_per_failure_to_reward_elements(
+        obs_smaller["trafo3w_loading_per_failure"], nminus1_definition.trafo3w_mask
+    )
+
+    for i in range(len(chronic_steps)):
+        chronic, step = chronic_steps[i]
+        obs_chronic_step = {key: obs_smaller[key][i] for key in keys_to_keep}
+        np.save(
+            destination_path / f"{chronic:04d}/{step}.npy",
+            obs_chronic_step,
+        )
+    return None
 
 
 def main(
@@ -104,26 +121,55 @@ def main(
     path_suffix = "nminus1_dc" if dc else "nminus1_ac"
     destination_path = destination_path / path_suffix
 
-    if not destination_path.exists():
-        destination_path.mkdir()
+    n_chronics = len(grid.chronics.n_timesteps)
+    async_results_list: List[AsyncResult] = []
+    chronic_steps: List[Tuple[int, int]] = []
+
+    for chronic in range(n_chronics):
+        for step in range(grid.chronics.n_timesteps[chronic]):
+            chronic_steps.append((chronic, step))
+
+    already_processed_chronic_steps = []
 
     # check for existing completed files and exclude them from processing
-    list_existing_files = list(destination_path.glob("*.npy"))
-    already_processed = [int(file.stem) for file in list_existing_files]
-    list_steps = np.arange(grid.chronics.n_timesteps)
-    list_steps = np.setdiff1d(list_steps, already_processed)
+    for chronic in range(n_chronics):
+        chronic_destination_path = destination_path / f"{chronic:04d}"
 
-    # give each process a list of timesteps
-    step_partitions = np.array_split(list_steps, n_procs)
+        if not chronic_destination_path.exists():
+            chronic_destination_path.mkdir(parents=True)
+            continue
 
-    async_results_list: List[AsyncResult] = []
+        list_existing_files = list(chronic_destination_path.glob("*.npy"))
+        already_processed_chronic_steps.extend(
+            [(chronic, int(file.stem)) for file in list_existing_files]
+        )
+
+    chronic_steps = [
+        chronic_step
+        for chronic_step in chronic_steps
+        if chronic_step not in already_processed_chronic_steps
+    ]
+    if not len(chronic_steps):
+        raise ValueError(
+            f"No new chronic steps to process. Try deleting {destination_path} and rerunning."
+        )
+    if n_procs == 1:
+        run_nminus1(
+            grid=grid, chronic_steps=chronic_steps, destination_path=destination_path
+        )
+        return
     with Pool(n_procs) as p:
-        for steps in step_partitions:
+        # give each process a list of timesteps
+        chronic_step_chunks = np.array_split(chronic_steps, n_procs)
+
+        for chronic_step_chunk in chronic_step_chunks:
             async_results_list.append(
                 p.apply_async(
                     run_nminus1,
                     kwds=dict(
-                        grid=grid, steps=steps, destination_path=destination_path
+                        grid=grid,
+                        chronic_steps=chronic_step_chunk,
+                        destination_path=destination_path,
                     ),
                 )
             )
@@ -132,7 +178,6 @@ def main(
                 [async_result.get() for async_result in async_results_list]
         else:
             [async_result.get() for async_result in async_results_list]
-        print("Finished all processes")
 
 
 if __name__ == "__main__":
--- examples/nminus1_analysis/process_nminus1_calculations.py
+++ examples/nminus1_analysis/process_nminus1_calculations.py
@@ -2,10 +2,10 @@ import argparse
 import warnings
 from functools import partial
 from itertools import chain
+from logging import getLogger
 from multiprocessing import Pool, cpu_count
-from multiprocessing.pool import AsyncResult
 from pathlib import Path
-from typing import Dict, Iterable, List
+from typing import Dict, List
 
 import numpy as np
 import pandas as pd
@@ -14,11 +14,12 @@ from tqdm import tqdm
 from grid2vec.grid import Grid, NMinus1Definition, load_grid
 
 warnings.simplefilter("ignore", category=FutureWarning)
+logger = getLogger(__name__)
 
 
 def process_nminus1_observation_file(
     file: Path, grid: Grid, crit_threshold: float = 90.0
-) -> List[Dict[str, float]]:
+) -> List[Dict[str, float | str]]:
     """Takes a single observation file from the powerflow analysis and returns a list
     of dicts containing the relevant information for the overloads.
 
@@ -32,81 +33,68 @@ def process_nminus1_observation_file(
     """
     obs: dict = np.load(file, allow_pickle=True).item()
 
-    line_loading_per_failure = obs["line_loading_per_failure"]
-    trafo_loading_per_failure = obs["trafo_loading_per_failure"]
-    trafo3w_loading_per_failure = obs["trafo3w_loading_per_failure"]
-
-    # get grid indices instead of numpy element indices
-    grid_line_index = grid.net.line.loc[pd.Series(grid.line_for_reward)].index
-    grid_line_mapping = {i: l for i, l in enumerate(grid_line_index)}
-
-    grid_trafo_index = grid.net.trafo.loc[pd.Series(grid.trafo_for_reward)].index
-    grid_trafo_mapping = {i: t for i, t in enumerate(grid_trafo_index)}
-
-    grid_trafo3w_index = grid.net.trafo3w.loc[pd.Series(grid.trafo3w_for_reward)].index
-    grid_trafo3w_mapping = {i: t for i, t in enumerate(grid_trafo3w_index)}
-
     if grid.nminus1_definition is not None:
         nminus1_definition: NMinus1Definition = grid.nminus1_definition
     else:
         raise ValueError(
-            "No nminus1 definition found. Please run the analysis with nminus1=True"
+            "No nminus1 definition found. Please re-run the analysis with nminus1=True"
         )
 
-    # get indices of failures
-    failure_grid_indices = (
-        grid.net.line.loc[pd.Series(nminus1_definition.line_mask)].index.tolist()
-        + grid.net.trafo.loc[pd.Series(nminus1_definition.trafo_mask)].index.tolist()
-        + grid.net.trafo3w.loc[
-            pd.Series(nminus1_definition.trafo3w_mask)
-        ].index.tolist()
-    )
+    # get mapping of nminus1 element ids to pandapower element ids for each element type
+    nminus1_to_pandapower_element_mapping = {}
+    for element_type in ["line", "trafo", "trafo3w"]:
+        # index of the pandapower element ids that are relevant for the nminus1 analysis
+        # by taking the True values from the nminus1 mask
+        pandapower_nminus1_index = getattr(grid.net, element_type).index[
+            getattr(nminus1_definition, f"{element_type}_mask")
+        ]
 
-    # put asset type according to which position in the array it is
-    failure_asset_types = (
-        ["line"] * int(nminus1_definition.line_mask.sum())
-        + ["trafo"] * int(nminus1_definition.trafo_mask.sum())
-        + ["trafo3w"] * int(nminus1_definition.trafo3w_mask.sum())
-    )
-    df_failure_mapping = pd.DataFrame(
-        {"grid_id": failure_grid_indices, "failure_asset_type": failure_asset_types}
-    )
+        # create a simple dict mapping the nminus1 array element ids to the pandapower element ids
+        nminus1_to_pandapower_element_mapping[element_type] = {
+            i: e for i, e in enumerate(pandapower_nminus1_index)
+        }
 
-    results = []
-    for (
-        element_type,
-        loading_per_failure_array,
-        grid_index_mapping,
-    ) in zip(
-        ["line", "trafo", "trafo3w"],
-        [
-            line_loading_per_failure,
-            trafo_loading_per_failure,
-            trafo3w_loading_per_failure,
-        ],
-        [grid_line_mapping, grid_trafo_mapping, grid_trafo3w_mapping],
-    ):
-        # get indices of overloads
+    results: List[Dict[str, float | str]] = []
+    for element_type in ["line", "trafo", "trafo3w"]:
+        # get relevant mapping created above
+        pandapower_nminus1_element_mapping = nminus1_to_pandapower_element_mapping[
+            element_type
+        ]
+        # get array of loading values for each failure
+        loading_per_failure_array = obs[f"{element_type}_loading_per_failure"]
+
+        # get the indices (failed element, overloaded element) of the overloads
         overload_array = np.argwhere(loading_per_failure_array >= crit_threshold)
+
         for overload in overload_array:
+            failed_element_id = overload[0]
+            overloaded_element_id = overload[1]
+
+            nminus1_element_type = nminus1_definition[failed_element_id][0].name.lower()
             return_dict = {
-                "vec_env": overload[0],
+                "chronic": file.parent.stem,
                 "step": file.stem,
-                "nminus1_failure_type": df_failure_mapping.loc[
-                    overload[1], "failure_asset_type"
-                ],
-                "nminus1_failure_id": df_failure_mapping.loc[overload[1], "grid_id"],
+                "nminus1_failure_type": nminus1_element_type,
+                "nminus1_failure_id": nminus1_to_pandapower_element_mapping[
+                    nminus1_element_type
+                ][failed_element_id],
                 "overloaded_element_type": element_type,
-                "overloaded_element_id": grid_index_mapping[overload[2]],
+                "overloaded_element_id": pandapower_nminus1_element_mapping[
+                    overloaded_element_id
+                ],
                 "loading_value": loading_per_failure_array[
-                    overload[0], overload[1], overload[2]
+                    failed_element_id, overloaded_element_id
                 ].item(),
             }
             results.append(return_dict)
+
+    if not len(results):
+        logger.warning(f"No overloads found in {file}")
+
     return results
 
 
-def main(data_path: Path, nminus1: bool, dc: bool, n_procs: int, crit_threshold: float):
+def main(data_path: Path, dc: bool, n_procs: int, crit_threshold: float):
     """Takes all of the completed .npy files from the analysis produced by collect_powerflow_calculations.py
     and collects them into a csv file with the overloads.
 
@@ -119,35 +107,33 @@ def main(data_path: Path, nminus1: bool, dc: bool, n_procs: int, crit_threshold:
 
     Writes the completed file to data_path/powerflow_analysis/{path_suffix}/overloads.csv
     """
-    grid = load_grid(data_path, nminus1=nminus1, dc=dc)
+    grid = load_grid(data_path, nminus1=True, dc=dc)
     path_suffix = "nminus1_dc" if dc else "nminus1_ac"
     analysis_path = data_path / "powerflow_analysis" / path_suffix
-    list_files = list(analysis_path.glob("*.npy"))
+    list_files = list(analysis_path.glob("*/*.npy"))
+
     if not len(list_files):
         raise ValueError(
             f"No analysis files found in {analysis_path}. Try running collect_powerflow_calculations.py first."
         )
-    results: Iterable[AsyncResult] = []
-    if nminus1:
-        with Pool(n_procs) as p:
-            results = list(
-                tqdm(
-                    p.imap(
-                        partial(
-                            process_nminus1_observation_file,
-                            grid=grid,
-                            crit_threshold=crit_threshold,
-                        ),
-                        list_files,
-                    )
+    with Pool(n_procs) as p:
+        results = list(
+            tqdm(
+                p.imap(
+                    partial(
+                        process_nminus1_observation_file,
+                        grid=grid,
+                        crit_threshold=crit_threshold,
+                    ),
+                    list_files,
                 )
             )
-    else:
-        raise NotImplementedError("Not implemented yet")
+        )
+
     # flatten list of lists
-    results = list(chain(results))
+    results = chain.from_iterable(results)  # type: ignore
     df_results = pd.DataFrame(results)
-    df_results.to_csv(analysis_path / "overloads.csv", index=False)
+    df_results.to_csv(analysis_path / "nminus1_overloads.csv", index=False)
 
 
 if __name__ == "__main__":
